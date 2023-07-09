ac5d2e142bb10c1233d85f688fb310e0ad5ea5d9
Kushtar <Amalbekov312@gmail.com>
2023-07-09 19:24:25 +0600

    MaximumProductSubarray_152: Looked up.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
 import org.example.problems.arrays.ContainsDuplicate_217;
+import org.example.problems.arrays.MaximumProductSubarray_152;
 
 public class Main {
     public static void main(String[] args) {
 
-        boolean contains = ContainsDuplicate_217.hasDuplicates();
-        System.out.println(contains);
+        int max = MaximumProductSubarray_152.maxProductSubArray();
+        System.out.println(max);
     }
 }
